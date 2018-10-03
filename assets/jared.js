c3f764ae0f55d0b3e991a315f2f9935377acff33
Jared Smith <jsmith1088@gmail.com>
2018-10-03 10:08:25 -0500

    added modal and some styling

--- assets/jared.js
+++ assets/jared.js
@@ -7,3 +7,16 @@ let showFilter = function(event){
 
 
 $(".showFilter").on("click",showFilter);
+
+//bring the menu back based upon clicking now
+let showMenu = function(event){
+    //prevent the default event
+    event.preventDefault();
+    //unhide navbar
+    $('.navbar').toggleClass('hidden');
+    //hide initial view of application
+    $('.firstView').hide();
+}
+
+//Show navigation bar and filter button when you click on the Now button in initial view.
+$(".nowButton").on("click",showMenu)
