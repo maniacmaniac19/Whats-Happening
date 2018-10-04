c4a2784554667f2019a67e90428ddba80d3cbf74
ZacSol <42217556+ZacSol@users.noreply.github.com>
2018-10-04 11:11:53 -0500

    updating master

--- assets/app.js
+++ assets/app.js
@@ -39,28 +39,37 @@ const savePosition=function(position){
 }
 
 //Jared's Code
-    let showFilter = function(event){
-        event.preventDefault(); 
-        console.log("yes")
-        $('.filter').toggleClass('hidden')
-        
-    }
+let showFilter = function (event) {
+    event.preventDefault();
+    console.log("yes")
+    $('.filter').toggleClass('hidden')
+
+}
 
 
-    $(".showFilter").on("click",showFilter);
+$(".showFilter").on("click", showFilter);
 
-    //bring the menu back based upon clicking now
-    let showMenu = function(event){
-        //prevent the default event
-        // event.preventDefault();
-        //unhide navbar
-        $('.navbar').toggleClass('hidden');
-        //hide initial view of application
-        $('.firstView').hide();
-    }
+//bring the menu back based upon clicking now
+let showMenu = function (event) {
+    //prevent the default event
+    // event.preventDefault();
+    //unhide navbar
+    $('.navbar').toggleClass('hidden');
+    //hide initial view of application
+    $('.firstView').hide();
+}
 
-    //Show navigation bar and filter button when you click on the Now button in initial view.
-    // $(".nowButton").on("click",showMenu);
+//Show navigation bar and filter button when you click on the Now button in initial view.
+// $(".nowButton").on("click",showMenu);
+//Show navigation bar and filter button when you click on the Now button in initial view.
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
 //End Jared's code
 
 
