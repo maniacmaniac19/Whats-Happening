69714ae786905facf8396bba311bb39cb5c37e02
Ruth Carthen <rcarthen@gmail.com>
2018-10-05 11:15:35 -0500

    google maps function edit

--- map.js
+++ map.js
@@ -39,19 +39,3 @@ $("#getLocation").on("click", function (event){
 });
 
 
-
-
-
-
-// var map;
-// function initMap() {
-//     var location = {lat: -25.363 , lng: 131.044};
-//     map = new google.maps.Map(document.getElementById("map"), { 
-//         zoom: 4,
-//     center: location
-//     });
-// }
-
-
-
-//getBounds()
